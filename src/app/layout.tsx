a41cee7e8664f23cec9a5ebea2d585ed36f706b4
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-18 00:04:53 +0530

    initial scale try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=0.9"/>
+      <meta name="viewport" content="width=device-width, initial-scale=0.8"/>
 
         {/* <link rel="icon" href="/jsm-logo.png" sizes="any" /> */}
         {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> */}
